9ae1ccb35198e78783129a5dd2fd0ca02d67ddcd
diegojsk <diego.jun@thunderatz.org>
2019-06-03 20:25:18 -0300

    Add equacoes.m (developing)

--- equacoes.m
+++ equacoes.m
@@ -1,19 +1,41 @@
-function ydot = dydt(t,y)
+function xp = x(t,x)
 % initialize ydot array
- global k m Gap Force
- ydot = zeros(4,1);
-%
- F1=k(1)*(y(1)-Gap(1));
- if Gap(1)~=0 && y(1)<=Gap(1)
-   F1=0;
- end 
- F2=k(2)*(y(3)-y(1)-Gap(2));
- if Gap(2)~=0 && y(3)<=Gap(2)
-   F2=0;
- end 
- ydot(1)=y(2);
- ydot(3)=y(4);
+ global k m c F
+ xp = zeros(12,1);
+
+%definindo os estados
+ %x(1) : x.inferior
+ %x(2) : vel.inferior
+ %x(3) : x.tronco
+ %x(4) : vel.tronco
+ %x(5) : thetha.tronco
+ %x(6) : vel.ang.tronco
+ %x(7) : x.cabeça
+ %x(8) : vel.cabeça
+ %x(9) : y.cabeça
+ %x(10) : vel.y.cabeça
+ %x(11) : thetha.cabeça
+ %x(12) : vel.ang.cabeça
  %
- ydot(2)=1/m(1)*(-F1+F2); 
- ydot(4)=-F2/m(2);
+ 
+ xp(1)=x(2);
+ xp(2)=(1/m(1))*(F(1) - c(1)*x(2) - k(1)*x(1));
+ %
+ xp(3)=x(4);
+ xp(4)=;
+ %
+ xp(5)=x(6);
+ xp(6)=;
+ %
+ xp(7)=x(8);
+ xp(8)=;
+ %
+ xp(9)=x(10);
+ xp(10)=;
+ %
+ xp(11)=x(12);
+ xp(12)=;
+ %
+ 
+ 
 end
