7436039adde741f89473fadb5b710a9bbb7a127e
diegojsk <diego.jun@thunderatz.org>
2019-06-20 16:05:38 -0300

    Fix main_errado

--- Script/main_errado.m
+++ Script/main_errado.m
@@ -11,20 +11,33 @@ m_t = 44.5;
 J_t = 1.5;
 L_t = 0.35;
 L_p = 0.5;
+
+%k_ab = 0;
+%c_ab = 0;
 k_ab = 104;
 c_ab = 50.4;
+
 k_p = 20200;
 c_p = 266;
 k_rp = 915;
 c_rp = 34;
+
 k_s = 1815;
 c_s = 568;
+%k_s = 0;
+%c_s = 0;
+
+
 k_b = 0;
 c_b = 0;
 k_ri = 200;
 c_ri = 724;
+
 k_i = 1815;
 c_i = 200;
+%k_i = 0;
+%c_i = 0;
+
 a = 90;
 g = 9.8;
 
@@ -33,7 +46,7 @@ g = 9.8;
 
 %dYdt = Equations_4(t,Y,m_i,m_c,J_c,m_t,J_t,L_t,L_p,k_ab,c_ab,k_p,c_p,k_rp,c_rp,k_s,c_s,k_b,c_b,k_ri,c_ri,k_i,c_i,a,g)
 
-tspan = 0:0.05:2;
+tspan = 0:0.01:1;
 %options = odeset('RelTol',1e-5); 
 Y0 = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
 % x_i: Y(1) Y(2)
@@ -57,7 +70,7 @@ sistema = @(t,Y)[(Y(2));
           ((1/J_c)*(-k_rp*(Y(11) - Y(5)) - c_ri*(Y(12) - Y(6))))];
 
 [t, Y] = ode45(sistema, tspan, [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0]);
-
+Y
 figure(1)
 plot(t, Y)
 grid on
